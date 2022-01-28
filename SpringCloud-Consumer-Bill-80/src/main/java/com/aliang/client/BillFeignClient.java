91f2c51f66096fbfe2b51ac9c629df1ba73d3918
Aliang99 <1294470230@qq.com>
2022-01-29 03:16:31 +0800

    指定配置类，用于在控制台打印日志

--- SpringCloud-Consumer-Bill-80/src/main/java/com/aliang/client/BillFeignClient.java
+++ SpringCloud-Consumer-Bill-80/src/main/java/com/aliang/client/BillFeignClient.java
@@ -1,43 +1,41 @@
 package com.aliang.client;
 
 import com.aliang.Common.CommonVo;
+import com.aliang.config.FeignConfiguration;
 import com.aliang.pojo.Bill;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 
 
-@FeignClient("Bill-Service")
+@FeignClient(value = "Bill-Service",configuration = FeignConfiguration.class)
 public interface BillFeignClient {
 
 
     @GetMapping("/bill/test")
     @ResponseBody
-    public String test();
+    String test();
 
     /**
      * 分页+条件查
      *
-     * @param pageNum
-     * @param pageSize
      * @return 状态： OK
      */
     @GetMapping("/bill/page")
     @ResponseBody
-    public CommonVo page(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
-                            @RequestParam(value = "pageSize",defaultValue = "2") int pageSize,
-                            Bill bill) ;
+    CommonVo page(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                  @RequestParam(value = "pageSize", defaultValue = "2") int pageSize,
+                  Bill bill) ;
 
 
     /**
      * 添加账单信息
      *
-     * @param bill
      * @return 状态： OK
      */
     @PutMapping("/bill/add")
     @ResponseBody
-    public CommonVo add(Bill bill) ;
+    CommonVo add(Bill bill) ;
 
 
     /**
@@ -47,37 +45,34 @@ public interface BillFeignClient {
      */
     @GetMapping("/bill/getTypes")
     @ResponseBody
-    public CommonVo getTypes() ;
+    CommonVo getTypes() ;
 
 
     /**
      * 根据id查询
      *
-     * @param id
      * @return 状态： NO
      */
     @GetMapping("/bill/queryById/{id}")
     @ResponseBody
-    public CommonVo queryById(@PathVariable("id") Long id) ;
+    CommonVo queryById(@PathVariable("id") Long id) ;
 
     /**
      * 提交修改信息
      *
-     * @param bill
      * @return 状态： NO
      */
     @PostMapping("/bill/update")
     @ResponseBody
-    public CommonVo update(Bill bill) ;
+    CommonVo update(Bill bill) ;
 
     /**
      * 根据id删除bill
      *
-     * @param id
      * @return 状态： NO
      */
     @DeleteMapping("/bill/delete/{id}")
     @ResponseBody
-    public CommonVo delete(@PathVariable("id") Long id) ;
+    CommonVo delete(@PathVariable("id") Long id) ;
 
 }
